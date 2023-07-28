72d14e5a9c3162aacd276a35a6921b7c8745e091
Mary <omota4fidelity@gmail.com>
2023-07-28 21:24:59 +0100

    completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function add(a, b){
+    return (a + b);
+}
+
+function subtract(a, b){
+    return (a - b);
+}
+
+function multiply(a, b){
+    return (a * b);
+}
+
+function divide(a, b){
+    return (a / b);
+}
+
+function increment(n){
+    n++
+    return n;
+}
+
+function decrement(n){
+    n--
+    return n;
+}
+
+function makeInt(n){
+    parseInt("n");
+    parseInt("n", 10);
+    parseInt("n!", 10);
+    return(parseInt(n, 10));
+}
+console.log(parseInt("n"));
+
+
+function preserveDecimal(n){
+    parseFloat("n");
+    return parseFloat(n);
+}
